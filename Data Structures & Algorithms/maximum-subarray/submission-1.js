fad4cb85326811458ae703ca1071f185381ee8e4
SmarakMishra1998 <67054653+SmarakMishra1998@users.noreply.github.com>
2026-07-14 19:14:06 +0530

    Add: maximum-subarray - submission-1

---
+++ Data Structures & Algorithms/maximum-subarray/submission-1.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    maxSubArray(nums) {
+        let maxSum = nums[0];
+        let currSum = nums[0];
+        let left = 0;
+        let right = 1;
+        let tempStart = 0;
+        for(let i=1; i< nums.length; i++){
+            if(nums[i] > nums[i] + currSum){
+                tempStart = i
+            }
+            currSum = Math.max(nums[i], nums[i] + currSum);
+            if(maxSum < currSum){
+                maxSum = currSum;
+                right = i;
+                left = tempStart;
+            }
+            console.log({currSum, maxSum, left, right})
+        }
+
+        return maxSum
+    }
+}
